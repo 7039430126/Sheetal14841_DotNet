ba242eb4d150bb831e3d551aaf5705afb88d9d10
7039430126 <78349081+7039430126@users.noreply.github.com>
2021-05-20 17:40:03 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿use BookDB;
+select * from Book;
+select * from Member;
+select * from Review;
+select * from Publisher;
+select * from [dbo].[__EFMigrationsHistory]
+
+sp_help 'Publisher'
+sp_help 'Book'
+sp_help 'Member'
+sp_help 'Review'
+
